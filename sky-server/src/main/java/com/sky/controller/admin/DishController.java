480b6b687e9b8b64211d83d00b9b6bf5256c1845
xufeng <306885028@qq.com>
2024-02-23 23:14:44 +0800

    新增菜品

---
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -0,0 +1,32 @@
+package com.sky.controller.admin;
+
+import com.sky.dto.DishDTO;
+import com.sky.result.Result;
+import com.sky.service.DishService;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 菜品管理
+ * @author <306885028@qq.com>
+ * @since 2024/02/23 22:41
+ */
+@RestController
+@RequestMapping("/admin/dish")
+@Slf4j
+public class DishController {
+    @Autowired
+    DishService dishService;
+    @PostMapping
+    @ApiOperation("新增菜品")
+    public Result save(@RequestBody DishDTO dishDTO) {
+        log.info("新增菜品：{}",dishDTO);
+        dishService.saveWithFlavor(dishDTO);
+        return Result.success();
+    }
+}
